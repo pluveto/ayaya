6525cd0af7aa87a031caa7be5ff26ae5f4818b49
Pluveto <receding@protonmail.com>
2021-07-18 12:46:44 +0800

    docs: 增加 ApiDoc

---
+++ docs/apidoc.cs
@@ -0,0 +1,23 @@
+namespace Api
+{
+    namespace Http
+    {
+        // == User 用户 ==
+        // 用户与下层系统的关系
+        namespace User
+        {
+            
+            
+        }
+        // == Member 社区成员 ==
+        // 用户与其他用户的关系
+        namespace Member
+        {
+            
+        }
+        namespace Post
+        {
+            
+        }
+    }
+}
